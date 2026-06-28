8dc0169c251a255c66045528af45a09b31555e7b
SalSaif80 <135459491+SalSaif80@users.noreply.github.com>
2026-06-28 10:43:17 +0300

    Update api.php

--- routes/api.php
+++ routes/api.php
@@ -61,6 +61,7 @@ Route::get('/user/{id}', function (string $id) {
 Route::get('teaPosts',[TeaPostController::class,'index']);
 
 Route::get('teaPost/{id}',[TeaPostController::class,'show']);
+
 Route::post('teaPosts',[TeaPostController::class,'store']);
 Route::put("teaPost/{id}",[TeaPostController::class,'update']);
 Route::delete("teaPost/{id}",[TeaPostController::class,'delete']);
